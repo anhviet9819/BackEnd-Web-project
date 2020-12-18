bc8bd483e8d89ed31b6404efa62b49a9e7b03095
unknown <anhviet9819@gmail.com>
2020-12-18 17:04:59 +0700

    BE

--- src/main/java/com/example/intern/Application.java
+++ src/main/java/com/example/intern/Application.java
@@ -4,8 +4,10 @@ import com.example.intern.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
